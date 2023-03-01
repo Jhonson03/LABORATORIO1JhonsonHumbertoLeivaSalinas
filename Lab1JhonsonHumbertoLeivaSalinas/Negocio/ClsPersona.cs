a84361c9e57c72edb7ab65ecea15a3b1580f3c89
Jhonson03 <ls0303032022@unab.edu.sv>
2023-02-28 23:54:48 -0600

    Logica

--- Lab1JhonsonHumbertoLeivaSalinas/Negocio/ClsPersona.cs
+++ Lab1JhonsonHumbertoLeivaSalinas/Negocio/ClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab1JhonsonHumbertoLeivaSalinas.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace Lab1JhonsonHumbertoLeivaSalinas.Negocio
 {
     public class ClsPersona
     {
+        public String CalcularIMC(Persona per)
+        {
+            double Promedio = per.Peso / Math.Pow(per.Altura, 2);
+
+            if(Promedio > 25)
+            {
+                return $"tiene sobre peso";
+            }
+            else 
+            {
+                return $"tiene un peso ideal";
+            }
+        }
+
+        public String MayorDeEdad(Persona per)
+        {
+            if(per.Edad >= 18)
+            {
+                return $"Es mayor de edad";
+            }
+            else
+            {
+                return $"Es menor de edad";
+            }
+        }
     }
 }
